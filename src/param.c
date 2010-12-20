12aac014f1aea73147ae2c29385429f5ab74007d
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-12-20 19:43:11 +0600

    src/param.c: * MAC большими буквами; * пример MAC адреса изменён с AA-AA-... на XX-XX-...; * укорочено сообщение о неправильном MAC адресе

--- src/param.c
+++ src/param.c
@@ -150,17 +150,17 @@ void Set_Mac (void)
 		flags |= FLAG_OLD_MAC_VALID;
 		mactostr (mac, mactmp);
 		mactmp[17] = '\0';
-		buart_print ("\n\rCurrent mac addres: ");
+		buart_print ("\n\rCurrent MAC address: ");
 		buart_print (mactmp);
 #ifndef NO_NUMBER_OF_MACS
 		buart_print ("\n\r");
-		buart_print ("Number of mac address: ");
+		buart_print ("Number of MAC address: ");
 		buart_put (macnum + '0');
 #endif
 	}
 
 mac_again:
-	buart_print ("\n\rEnter new mac address (AA-AA-AA-AA-AA-AA): ");
+	buart_print ("\n\rEnter new MAC address (XX-XX-XX-XX-XX-XX): ");
 	buf[0] = 0;
 	ReadLine (buf, BOOT_LINE_SIZE);
 
@@ -170,7 +170,7 @@ mac_again:
 
 		if (i < 6 || mac[0] != 0x00)
 		{
-			buart_print ("Either the format of the input mac address or the mac address is incorrect.\n\r");
+			buart_print ("Incorrect MAC address!\n\r");
 			goto mac_again;
 		}
 
@@ -179,7 +179,7 @@ mac_again:
 
 num_again:
 #ifndef NO_NUMBER_OF_MACS
-	buart_print ("Enter new number of mac address (between 1-8): ");
+	buart_print ("Enter new number of MAC address (between 1-8): ");
 	newmacnum = buart_getchar ();
 	buart_put (newmacnum);
 
@@ -188,7 +188,7 @@ num_again:
 		newmacnum = newmacnum - '0';
 		if (newmacnum < 1 || newmacnum > 8)
 		{
-			buart_print ("\n\rNumber of mac adderesses must be in the range [1-8].\n\r");
+			buart_print ("\n\rNumber of MAC adderesses must be in the range [1-8].\n\r");
 			goto num_again;
 		}
 
@@ -208,7 +208,7 @@ num_again:
 
 		if (bsp_SetMac (mac, macnum) != 0)
 		{
-			buart_print ("\n\rFailed to change mac address.");
+			buart_print ("\n\rFailed to change MAC address.");
 		} else
 		{
 			memcpy (mactmp, cfg->mac, 6);
