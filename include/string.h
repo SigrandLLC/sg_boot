fb4576f3ebe623d86ab4015b607b62bfca6720c2
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-12-24 21:12:35 +0600

    include/string.h: добавлен прототип memcmp

--- include/string.h
+++ include/string.h
@@ -3,11 +3,13 @@
 
 #include <stdlib.h> // size_t
 
-void *memcpy (void *dest, const void *src, size_t n);
-void *memset (void *s, int c, size_t n);
-size_t strlen(const char *s);
-char *strcpy (char *dest, const char *src);
-char *strncpy(char *dest, const char *src, size_t n);
+void *memcpy  (void *dest, const void *src, size_t n);
+void *memset  (void *s, int c, size_t n);
+int   memcmp  (const void *s1, const void *s2, size_t n);
+
+size_t strlen (const char *s);
+char * strcpy (char *dest, const char *src);
+char * strncpy(char *dest, const char *src, size_t n);
 
 #endif
 
